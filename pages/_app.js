e45f3fa5a5662f5ad139bf66b293766e6af922c1
Ebukaar <ebukaar@gmail.com>
2023-10-05 18:57:54 +0100

    resolved firebase issue

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,12 @@
 import "@/styles/globals.css";
 import Navbar from "@/components/Navbar";
+import FirebaseClient from "./firebase/config";
 
 export default function App({ Component, pageProps }) {
   return (
     <>
       <Navbar />
+      <FirebaseClient/>
       {/* <Component {...pageProps} />; */}
     </>
   );
